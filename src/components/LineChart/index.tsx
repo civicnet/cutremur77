12b4a367d98b0a71fc1ea396b1100543d205729b
ClaudiuCeia <claudiu.ceia+1@gmail.com>
2020-03-04 10:44:40 +0200

    fix(app/timeline) add acceleration labels

--- src/components/LineChart/index.tsx
+++ src/components/LineChart/index.tsx
@@ -43,27 +43,41 @@ const ConnectedLineChart: React.FC<Props> = props => {
   return (
     <div style={{ ...props.style, flexDirection: "column" }}>
       {Object.keys(props.data.acceleration).map((key, idx) => (
-        <VictoryLine
-          key={key}
-          height={25}
-          width={400}
-          data={props.data.acceleration[key as AccelerationDirection].toJS()}
-          containerComponent={
-            <VictoryContainer responsive={true} height={25} />
-          }
-          padding={0}
-          style={{
-            data: {
-              stroke: accelColors[key as AccelerationDirection],
-              strokeWidth: 1.5
-            },
-            parent: {
+        <div>
+          <span
+            style={{
               position: "absolute",
               top: idx * 20,
-              zIndex: 3 - idx
+              left: "-70%",
+              fontSize: 12,
+              color: accelColors[key as AccelerationDirection],
+              pointerEvents: "none"
+            }}
+          >
+            Accelerație {key} (cm/s<sup>2</sup>)
+          </span>
+          <VictoryLine
+            key={key}
+            height={25}
+            width={400}
+            data={props.data.acceleration[key as AccelerationDirection].toJS()}
+            containerComponent={
+              <VictoryContainer responsive={true} height={25} />
             }
-          }}
-        />
+            padding={0}
+            style={{
+              data: {
+                stroke: accelColors[key as AccelerationDirection],
+                strokeWidth: 1.5
+              },
+              parent: {
+                position: "absolute",
+                top: idx * 20,
+                zIndex: 3 - idx
+              }
+            }}
+          />
+        </div>
       ))}
     </div>
   );
