6c65b71f1744f021214681cddd95d6449a054407
ClaudiuCeia <claudiu.ceia+1@gmail.com>
2020-03-04 09:21:08 +0200

    Updates

---
+++ precache-manifest.b2844a24ec07587c0776c5d64fb1038a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e1d794d08dfcc7d388b57265ac5f8d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24e66e261842f613a8bd",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "cd675bb45e9d9701c2be",
+    "url": "/static/js/2.af276a95.chunk.js"
+  },
+  {
+    "revision": "5d99681ba0ea14f646d85c349c5f44ad",
+    "url": "/static/js/2.af276a95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24e66e261842f613a8bd",
+    "url": "/static/js/main.9579a652.chunk.js"
+  },
+  {
+    "revision": "3c5363db6521710778bb",
+    "url": "/static/js/runtime-main.7b74253b.js"
+  }
+]);
